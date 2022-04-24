bf19a5e65ed408bed5a1b2590ef136038f245c2d
Damilola Mohammed <damilolamohammed2@hotmail.com>
2022-04-24 16:39:14 +0100

    automatic option select in mobile pix trial 3

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
-let country = ''
 let elementsArray = []; //new array to take in created elements
-let mySelect = document.querySelector('select')
-mySelect.addEventListener('click', () => {
-    country =  mySelect.options[mySelect.selectedIndex].value 
-});
+var mySelect = document.querySelector('#country')
+//add event listener to listen to each option within the select element
+ mySelect.addEventListener('change', () => {
+     var country = mySelect.options[mySelect.selectedIndex].value
+      alert(country)
+ });
 
 
-const buttonParent = document.querySelector(".buttonParent");
-buttonParent.addEventListener("click", function(e) {
-    const button = e.target.matches(".button, .button *");
-    if (button) { // If child is click
-    // Your code here
-    alert(country)
+document.querySelector("button").addEventListener("click", function() {
     fetch(`https://ws-public.interpol.int/notices/v1/red?nationality=${country}`)
    .then( res => res.json() )
    .then( rawData => {
@@ -58,63 +54,8 @@ buttonParent.addEventListener("click", function(e) {
        console.log(`error: ${err}`)
    } )
 
-}});
+});
 
-
-//   document.querySelector('button').addEventListener('click', function() {
-//     fetch(`https://ws-public.interpol.int/notices/v1/red?nationality=${country}`)
-//     .then( res => res.json() )
-//     .then( rawData => {
-//         let neededData = rawData._embedded.notices
-//         console.log(rawData)
-//         if(neededData.length === 0) {
-//             let profiles = document.querySelector('.profiles')
-//             profiles.textContent = `NOTICE NOT AVAILABLE, TRY AGAIN LATER.`
-//         }else{
-//             let profiles = document.querySelector('.profiles')
-//             profiles.textContent = ``
-
-//             //conditionanal syntax to remove created element from DOM on each click so as to clear Dom for the input of fresh data
-//             if(elementsArray.length !== 0) {
-//                 elementsArray.forEach( function(item, index, arr) {
-//                     item.remove();
-//                 })
-//                 elementsArray = [];
-//             }
-
-//             for( let i = 0; i < neededData.length; i++ ) {    
-//                 let newImg = document.createElement("img")
-//                 if(neededData[i]._links.thumbnail==undefined){ newImg.src = `./Thumbnail.jpeg` }
-//                 else{
-//                     newImg.src = neededData[i]._links.thumbnail.href
-//                 }
-
-//                 let newDiv = document.createElement("div")
-//                 let newName = document.createElement("p")
-//                 let newDob = document.createElement("p")
-
-//                 elementsArray.push(newDiv);
-//                 elementsArray.push(newImg);
-//                 elementsArray.push(newName);
-//                 elementsArray.push(newDob);
-
-//                 newName.textContent = getName(neededData[i])
-//                 newDob.textContent = neededData[i].date_of_birth
-
-//                 profiles.appendChild(newDiv)
-//                 newDiv.appendChild(newImg)
-//                 newDiv.appendChild(newName)
-//                 newDiv.appendChild(newDob)
-
-//                 newDiv.classList.add("divStyle")
-//                 document.querySelector('.options').style.borderRight = '0.5px solid rgb(155, 148, 148)'
-//             }
-//         }
-//     } )
-//     .catch( err => {
-//         console.log(`error: ${err}`)
-//     } )
-// })
 
 //FUNCTION TO DET NAME FROM rawData
 function getName(obj) {
